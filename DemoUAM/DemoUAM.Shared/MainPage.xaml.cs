60815990d9f8088231272c8854a3b11423c2cf21
rlbisbe <rober90@gmail.com>
2015-01-23 01:58:40 +0100

    Added the tasksync component to make it work

--- DemoUAM/DemoUAM.Shared/MainPage.xaml.cs
+++ DemoUAM/DemoUAM.Shared/MainPage.xaml.cs
@@ -1,21 +1,33 @@
-﻿using Windows.UI.Xaml;
+﻿using System.Collections.ObjectModel;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace DemoUAM
 {
     public sealed partial class MainPage : Page
     {
+        private TaskSync taskSync;
         private TaskListViewModel viewModel;
         public MainPage()
         {
             this.InitializeComponent();
+
             viewModel = new TaskListViewModel();
+            SyncTaskList(viewModel);
             this.DataContext = viewModel;
         }
 
         private void Agregar_Click(object sender, RoutedEventArgs e)
         {
+            taskSync.UploadTask(viewModel.NewTask);
             viewModel.Save();
+        }
+
+        private async void SyncTaskList(TaskListViewModel viewModel)
+        {
+            taskSync = new TaskSync();
+            var tasks = await taskSync.GetTasks();
+            viewModel.TaskList = new ObservableCollection<TaskModel>(tasks);
         }
     }
 }
---
+++ DemoUAM/DemoUAM.Shared/TaskSync.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace DemoUAM
+{
+    public class TaskSync
+    {
+        private string url = "http://uam-demo.azurewebsites.net/api/tasks";
+
+        public async void UploadTask(TaskModel task)
+        {
+            HttpClient client = new HttpClient();
+            var data = JsonConvert.SerializeObject(task);
+            StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.PostAsync(url, content);
+            response.EnsureSuccessStatusCode();
+        }
+
+        public async Task<List<TaskModel>> GetTasks()
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            String jsonResponse = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<List<TaskModel>>(jsonResponse);
+        }
+    }
+}
